ba76a20d3ef5d19024eac0d13d07df437a98a0c0
emiliano <emipaz1975@hotmail.com>
2024-02-27 12:37:53 -0300

    vista con get_object_or_404

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -9,15 +9,26 @@ def lista_post(request):
     return render (request, "blog/post/lista_post.html", {"posts": posts})
 
 
+# def detalle_post(request, id):
+    
+#     try:
+#         post = Post.objects.get(id = id)
+#     except Exception as e:
+#         print(e)
+#         raise Http404 (f"No Existe el post con id:{id}")
+    
+#     return render(request, 
+#                   template_name ="blog/post/detalle_post.html", 
+#                   context = {"post": post})
+
+### ALternativa al Try Except 
+from django.shortcuts import render, get_object_or_404
 
 def detalle_post(request, id):
-    
-    try:
-        post = Post.objects.get(id = id)
-    except Post.DoesNotExits:
-        raise Http404 (f"No Existe el post con id:{id}")
-    
+    post = get_object_or_404(Post, 
+                             id = id,
+                             estado = Post.Status.PUBLICADO)
+
     return render(request, 
                   template_name ="blog/post/detalle_post.html", 
                   context = {"post": post})
-
