5c270708c2f63a96f3924a6b6482eb96fcb213b0
emiliano <emipaz1975@hotmail.com>
2024-03-02 00:03:29 -0300

    mostrar ultimos post

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -11,3 +11,9 @@ def total_de_posts():
 @register.simple_tag()
 def hora():
     return datetime.now().strftime("%d/%m/%Y")
+
+
+@register.inclusion_tag('blog/post/ultimos_post.html')
+def mostrar_ultimos_posts(count=5):
+    ultimos_post = Post.publicados.order_by('-publicado')[:count]
+    return {"ultimos_post": ultimos_post}
