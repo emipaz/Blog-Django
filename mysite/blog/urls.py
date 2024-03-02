ae39061ca62f44d6af3bb601a293c510450ef5c9
emiliano <emipaz1975@hotmail.com>
2024-03-02 17:58:30 -0300

    Agregar servicio de feed RSM

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('<int:año>/<int:mes>/<int:dia>/<slug:post>', views.detalle_post  , name = 'detalle_post'),
     path("<int:post_id>/compartir/"                 , views.compartir_post, name = "compartir_post"),
     path("<int:post_id>/comentar/"                  , views.comentar_post , name = "comentar_post"),
+    path("feed/", LatestPostsFeed(), name="post_feed"),
 ]
