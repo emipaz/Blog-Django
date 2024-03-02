5b5cbcfbe06abd2a6d0b12b2e477d117ce4183b9
emipaz <65197702+emipaz@users.noreply.github.com>
2024-03-02 19:45:53 -0300

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -139,6 +139,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = "emipaz1975@gmail.com"
-EMAIL_HOST_PASSWORD = "lkos sisz cdci tzds"
+EMAIL_HOST_PASSWORD = "XXXXXXXXXXXXXXXX"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
