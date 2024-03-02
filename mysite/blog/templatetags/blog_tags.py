ecfaef6f508165ec3d22133e0e061ab9bcfbf2b3
emiliano <emipaz1975@hotmail.com>
2024-03-02 00:28:40 -0300

    post mas comentados

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,6 +1,7 @@
 from django import template
 from ..models import Post
 from  datetime import datetime
+from django.db.models import Count
 
 register = template.Library()
 
@@ -17,3 +18,9 @@ def hora():
 def mostrar_ultimos_posts(count=5):
     ultimos_post = Post.publicados.order_by('-publicado')[:count]
     return {"ultimos_post": ultimos_post}
+
+@register.simple_tag()
+def mas_comentados(count=5):
+    return Post.publicados.annotate(
+        total_comentarios = Count('comentarios')
+    ).order_by('-total_comentarios')[:count]
